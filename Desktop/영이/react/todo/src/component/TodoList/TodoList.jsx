b905ed06fa2806c7ac253ad87e13c29e05319139
HBDCHAEYOUNG <devyoon02@gmail.com>
2024-07-05 15:42:37 +0900

    todo completed!!

--- Desktop/영이/react/todo/src/component/TodoList/TodoList.jsx
+++ Desktop/영이/react/todo/src/component/TodoList/TodoList.jsx
@@ -1,20 +1,42 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AddTodo from "../AddTodo/AddTodo";
 import Todo from "../Todo/Todo";
 import styles from "./TodoList.module.css";
+import getFilteredItem from "../../lib/getFilteredItem";
+import readTodoFromLocalStroage from "../../lib/readTodoFromLocalStroage";
 
 export default function TodoList({ filter }) {
-  const [todos, setTodos] = useState([
-    { id: 0, text: "안녕하세요", status: "active" },
-    { id: 1, text: "감사해요", status: "active" },
-  ]);
-  const handleAdd = (todo) => setTodos([...todos, todo]);
-  const handleUpdate = (updated) =>
+  const [todos, setTodos] = useState(readTodoFromLocalStroage);
+
+  //readTodoFromLocalStroage() <-usestate 매번 호출됨
+
+  //아래 셋 모두 같은 의미, 1회 호출
+  //readTodoFromLocalStroage
+  //()=>readTodoFromLocalStroage()
+  //function(){return readTodoFromLocalStroage()}
+  //타입스크립트 제네릭 공부!
+
+  const handleAdd = (todo) => {
+    setTodos((prev) => [...prev, todo]);
+  };
+  const handleUpdate = (updated) => {
     setTodos(todos.map((item) => (item.id === updated.id ? updated : item)));
-  const handleDelete = (deleted) =>
+  };
+  const handleDelete = (deleted) => {
     setTodos((todos) => todos.filter((item) => item.id !== deleted.id));
+  };
+
+  // const [showTodos, setShowTodos] = useState([]);
+  // useEffect(() => {
+  //   setShowTodos(todos.filter(({ status }) => filter !== status));
+  // }, [todos, filter]);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   const filtered = getFilteredItem(todos, filter);
+
   return (
     <section className={styles.container}>
       <ul className={styles.list}>
@@ -30,10 +52,4 @@ export default function TodoList({ filter }) {
       <AddTodo onAdd={handleAdd} />
     </section>
   );
-}
-
-// !function... 물어보기
-function getFilteredItem(todos, filter) {
-  if (filter === "all") return todos;
-  return todos.filter((t) => t.status === filter);
 }
